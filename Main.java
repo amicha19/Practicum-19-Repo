d1cac6e278fa255863db5bcbaaead0cf34f1cce4
amicha19 <158494961+amicha19@users.noreply.github.com>
2024-02-13 08:51:26 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,40 +1,38 @@
-// Code for Practicum 19 (Arrays in Java)
-// (TO COMPLETE)
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) {
-    int sum;
-    double avg1, avg2;
-    
-    Scanner input = new Scanner(System.in);
-    
-    // declare an array of integers called nums of size 100
-    // (to complete)
-   
+    public static void main(String[] args) {
+        int sum = 0;
+        double avg1, avg2;
 
-    // create a for loop that populates array nums such that
-    // the first element is 100, the second element 101, etc.
-    // (to complete)
-    
-    
-    // use a for loop to determine the average of all of the
-    // values in array nums and assigns to variable avg1
-    // (to complete)
-    
+        Scanner input = new Scanner(System.in);
 
-    // use a for loop to add 5 to every elements in array nums
-    // (to complete)
-    
-    
-    // use a for loop to determine the average of all of the
-    // values in array nums and assigns to variable avg2
-    // (to complete)
-    
+        // declare an array of integers called nums of size 100
+        int[] nums = new int[100];
 
-    // display results
-    System.out.println("avg1 = " + avg1);
-    System.out.println("avg2 = " + avg2);
-    
-  }
+        // create a for loop that populates array nums and calculates avg1
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = 100 + i;
+            sum += nums[i];
+        }
+        avg1 = (double) sum / nums.length;
+
+        // add 5 to every element in array nums
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] += 5;
+        }
+
+        // reset sum for calculating avg2
+        sum = 0;
+
+        // use a for-each loop to calculate the sum for avg2
+        for (int num : nums) {
+            sum += num;
+        }
+        avg2 = (double) sum / nums.length;
+
+        // display results
+        System.out.println("avg1 = " + avg1);
+        System.out.println("avg2 = " + avg2);
+    }
 }
